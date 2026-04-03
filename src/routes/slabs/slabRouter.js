83de2c9a39dc3208c414bf635ab5944db7e898e6
unknown <salunkheshreya19@gmail.com>
2026-04-03 20:14:41 +0530

    chnage

--- src/routes/slabs/slabRouter.js
+++ src/routes/slabs/slabRouter.js
@@ -5,61 +5,165 @@ import { slabPopulateOptions } from "../../utils/constant.js";
 import { authenticateToken } from "../../middleware/auth.middleware.js";
 const slabRouter = Router();
 
-const slabsData = [
-  { index: 0, name: "On Booking", percent: 10 },
-  { index: 1, name: "On Execution of Agreement", percent: 10 },
-  { index: 2, name: "On Completion of Basement 1", percent: 5 },
-  { index: 3, name: "On Completion of Basement 2", percent: 5 },
-  { index: 4, name: "On Completion of Basement 3", percent: 5 },
-  { index: 5, name: "On Completion of Plinth Level", percent: 10 },
-  { index: 6, name: "On Completion of 1st Slab (Ground)", percent: 3 },
-  { index: 7, name: "On Completion of 2nd Slab", percent: 3 },
-  { index: 8, name: "On Completion of 3rd Slab", percent: 3 },
-  { index: 9, name: "On Completion of 4th Slab", percent: 3 },
-  { index: 10, name: "On Completion of 5th Slab", percent: 1 },
-  { index: 11, name: "On Completion of 6th Slab", percent: 1 },
-  { index: 12, name: "On Completion of 7th Slab", percent: 1 },
-  { index: 13, name: "On Completion of 8th Slab", percent: 1 },
-  { index: 14, name: "On Completion of 9th Slab", percent: 1 },
-  { index: 15, name: "On Completion of 10th Slab", percent: 1 },
-  { index: 16, name: "On Completion of 11th Slab", percent: 1 },
-  { index: 17, name: "On Completion of 12th Slab", percent: 1 },
-  { index: 18, name: "On Completion of 13th Slab", percent: 1 },
-  { index: 19, name: "On Completion of 14th Slab", percent: 1 },
-  { index: 20, name: "On Completion of 15th Slab", percent: 0.5 },
-  { index: 21, name: "On Completion of 16th Slab", percent: 0.5 },
-  { index: 22, name: "On Completion of 17th Slab", percent: 0.5 },
-  { index: 23, name: "On Completion of walls", percent: 0.5 },
-  { index: 23, name: "On Completion of internal plaster", percent: 1 },
+const slabsData =[
+  {
+    "index": 1,
+    "name": "On Booking",
+    "percent": "10"
+  },
+  {
+    "index": 2,
+    "name": "On Execution of Agreement",
+    "percent": "15"
+  },
+  {
+    "index": 3,
+    "name": "On Completion of Plinth level",
+    "percent": "20"
+  },
+  {
+    "index": 4,
+    "name": "On completion of 1st slab",
+    "percent": "3"
+  },
+  {
+    "index": 5,
+    "name": "On completion of 2nd slab",
+    "percent": "3"
+  },
+  {
+    "index": 6,
+    "name": "On completion of 3rd slab",
+    "percent": "3"
+  },
+  {
+    "index": 7,
+    "name": "On completion of 4th slab",
+    "percent": "3"
+  },
+  {
+    "index": 8,
+    "name": "On completion of 5th slab",
+    "percent": "3"
+  },
+  {
+    "index": 9,
+    "name": "On completion of 6th slab",
+    "percent": "3"
+  },
+  {
+    "index": 10,
+    "name": "On completion of 7th slab",
+    "percent": "3"
+  },
+  {
+    "index": 11,
+    "name": "On Completion of walls",
+    "percent": "1.45"
+  },
+  {
+    "index": 12,
+    "name": "On Completion of internal plaster",
+    "percent": "1.45"
+  },
+  {
+    "index": 13,
+    "name": "On Completion of Flooring",
+    "percent": "1.45"
+  },
+  {
+    "index": 14,
+    "name": "On Completion of Doors",
+    "percent": "1.45"
+  },
+  {
+    "index": 15,
+    "name": "On Completion of windows",
+    "percent": "1.45"
+  },
+  {
+    "index": 16,
+    "name": "On Completion of Sanitary fittings",
+    "percent": "1.45"
+  },
+  {
+    "index": 17,
+    "name": "On Completion of Staircase",
+    "percent": "1.45"
+  },
+  {
+    "index": 18,
+    "name": "On Completion of Lift wall",
+    "percent": "1.45"
+  },
+  {
+    "index": 19,
+    "name": "On Completion of Lobbies",
+    "percent": "1.45"
+  },
+  {
+    "index": 20,
+    "name": "On Completion of external plumbing",
+    "percent": "1.45"
+  },
+  {
+    "index": 21,
+    "name": "On Completion of External plaster",
+    "percent": "1.45"
+  },
+  {
+    "index": 22,
+    "name": "On Completion of elevation",
+    "percent": "1.45"
+  },
+  {
+    "index": 23,
+    "name": "On Completion of waterproofing",
+    "percent": "1.45"
+  },
+  {
+    "index": 24,
+    "name": "On Completion of terrace",
+    "percent": "1.45"
+  },
+  {
+    "index": 25,
+    "name": "On Completion of Lift",
+    "percent": "1.45"
+  },
+  {
+    "index": 26,
+    "name": "On Completion of water pumps",
+    "percent": "1.45"
+  },
+  {
+    "index": 27,
+    "name": "On Completion of electrical\nfittings",
+    "percent": "1.45"
+  },
+  {
+    "index": 28,
+    "name": "On Completion of electro,\nmechanical and environmental requirements",
+    "percent": "1.45"
+  },
+  {
+    "index": 29,
+    "name": "On completion of entrance lobby",
+    "percent": "1.45"
+  },
+  {
+    "index": 30,
+    "name": "On completion of plinth\nprotection and paving",
+    "percent": "1.45"
+  },
+  {
+    "index": 31,
+    "name": "On Possession",
+    "percent": "5"
+  }
 
-  { index: 24, name: "On Completion of Flooring", percent: 1 },
-  { index: 25, name: "On Completion of Doors", percent: 1 },
-  { index: 26, name: "On Completion of Windows", percent: 1 },
-  { index: 27, name: "On Completion of Sanitary fittings", percent: 1.25 },
-  { index: 28, name: "On Completion of Staircase", percent: 1.25 },
-  { index: 29, name: "On Completion of Lift well", percent: 1.25 },
-  { index: 30, name: "On Completion of Lobbies", percent: 1.25 },
-  { index: 31, name: "On Completion of external plumbing", percent: 1 },
-  { index: 32, name: "On Completion of External plaster", percent: 1 },
-  { index: 33, name: "On Completion of elevation", percent: 1 },
-  { index: 34, name: "On Completion of terrace", percent: 1 },
-  { index: 35, name: "On Completion of Waterproofing", percent: 1 },
-  { index: 36, name: "On Completion of Lift", percent: 4 },
-  { index: 37, name: "On Completion of water pumps", percent: 1 },
-  { index: 38, name: "On Completion of electrical fittings", percent: 1 },
-  {
-    index: 39,
-    name: "On Completion of electrical, mechanical and environmental requirements",
-    percent: 1,
-  },
-  { index: 40, name: "On Completion of entrance lobby", percent: 2 },
-  {
-    index: 41,
-    name: "On completion of plinth protection and paving",
-    percent: 1,
-  },
-  { index: 42, name: "On Possession", percent: 5 },
 ];
+
 slabRouter.post("/add-slab", async (req, res) => {
   const { slabs, project, currentSlab } = req.body;
   try {
@@ -69,8 +173,8 @@ slabRouter.post("/add-slab", async (req, res) => {
     //   return res.send(errorRes(401, "slabs required require3d"));
 
     const newSlabs = slabsData.map((slb, i) => {
-      slb.buildingNo = 2;
-      slb.id = `${project}-2-${i}-${slb.name?.replace(
+      slb.buildingNo = 3;
+      slb.id = `${project}-3-${slb.index}-${slb.name?.replace(
         /\s+/g,
         "-",
       )}`.toLowerCase();
@@ -79,7 +183,7 @@ slabRouter.post("/add-slab", async (req, res) => {
     });
 
     const proj = await slabModel.findByIdAndUpdate(
-      "slab-project-ev23-malibu-west-koparkhairne-2024",
+      "slab-project-solaris-rohinjan-2025",
       {
         $addToSet: {
           slabs: newSlabs,
@@ -87,7 +191,7 @@ slabRouter.post("/add-slab", async (req, res) => {
       },
     );
 
-    // const resp = await slabModel.create({
+    // const proj = await slabModel.create({
     //   _id: `slab-${project}`,
     //   ...req.body,
     //   slabs: newSlabs,
