33789f2ff58e5cdd3477ad0c0d02de557cb36118
Tech2cool <aktarvishwas8520@gmail.com>
2026-04-22 21:36:35 +0530

    oka2

--- src/routes/redev10Mb/redev10mbRouter.js
+++ src/routes/redev10Mb/redev10mbRouter.js
@@ -6,7 +6,7 @@ const redev10mbRouter = Router();
 
 redev10mbRouter.post("/add-enquiry-10mb-redev", async (req, res) => {
   //
-  const { name, email, phoneNumber, countryCode } = req.body;
+  const { name, email, phoneNumber, countryCode, ignoreEmail } = req.body;
   try {
     if (!phoneNumber || !name) {
       return errorRes2(res, 401, "name, phoneNumber is required");
@@ -15,24 +15,25 @@ redev10mbRouter.post("/add-enquiry-10mb-redev", async (req, res) => {
       //
       ...req.body,
     });
-
-    try {
-      await sendMultipleEmail(
-        ["ricki@evgroup.co.in"],
-        `10 Marina Rehab Lead Received for`,
-        `
-        Hi,
-        A new client has submitted a form. Here are the details:
-        
-        Client Name: ${name}
-        Phone Number: ${phoneNumber}
-        Email Address: ${email}
-            
-        Best regards,
-        EV Homes Construction Pvt Ltd`,
-      );
-    } catch (error) {
-      //
+    if (ignoreEmail == true) {
+      try {
+        await sendMultipleEmail(
+          ["ricki@evgroup.co.in"],
+          `10 Marina Rehab Lead Received for`,
+          `
+                Hi,
+                A new client has submitted a form. Here are the details:
+                
+                Client Name: ${name}
+                Phone Number: ${phoneNumber}
+                Email Address: ${email}
+                
+                Best regards,
+                EV Homes Construction Pvt Ltd`,
+        );
+      } catch (error) {
+        //
+      }
     }
     //
     return successRes2(res, 200, "enquiry sent", { data: data });
